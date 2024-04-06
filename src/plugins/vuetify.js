392bf20d38b70764f72dd76597c83fb47eca54ba
AlexandreOliveira7 <serolfalexandre@gmail.com>
2024-04-06 19:09:09 -0300

    configuracoes-rota-e-cores

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,6 +14,14 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primaria: '#00008B',
+          gelo: '#f5f5f5',
+          cinza: '#bebebe'
+        }
+      }
+    }
   },
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
-
-/**
- * router/index.ts
- *
- * Automatic routes for `./src/pages/*.vue`
- */
-
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import LayoutDefault from '@/layouts/default/Default.vue'
+import ViewHome from '@/views/HomePage.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: LayoutDefault,
+    children: [
+      {
+        path: '/',
+        name: 'Home',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: ViewHome,
+      },           
+    ],
+  },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes,
 })
 
 export default router
